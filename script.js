8197fe98f5c0183eb56d6694bf3bea9098e77071
nishchal singh <singhnishchal27@gmail.com>
2024-10-16 23:04:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 //your JS code here. If required.
 async function getArray(){
 	let array = [1,2,3,4]
-	return array;
-}
+	return array
+	
+} 
+
 
 getArray().then((array)=>{
-	let evens = array.filter((i) => i%2 === 0);
-	setTimeOut(()=>{
-		let el = document.getElementById('output');
-		el.innerHtml = evens.join(',');
-	}, 1000);
-
-	return evens;
+	let evens = array.filter((i)=>i%2===0)
+    
+    setTimeout(()=>{
+      let el = document.getElementById('output');
+      el.innerHTML = evens.join(',')
+    },1000)
+    
+	return evens
 }).then((evens)=>{
-	let twos = evens.map((i)=>i*2);
-  setTimeOut(()=>{
+  let twos = evens.map((i)=>i*2)
+  setTimeout(()=>{
     let el = document.getElementById('output');
-    el.innerHTML = twos.join(',');
+      el.innerHTML = twos.join(',')
   },2000)
 })
